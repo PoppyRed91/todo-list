6bad5786e12ebb4f3ade31d7719c27abde5aebeb
setayeshnri <setayeshn1382@gmail.com>
2024-03-19 16:57:33 +0100

    routing second edition

--- client/src/LandingPage/LandingPage.jsx
+++ client/src/LandingPage/LandingPage.jsx
@@ -1,16 +1,20 @@
 import styles from "./LandingPage.css";
 import image from "./TaskZilla.png";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   return (
     <>
-    <div className="landing">
-      <img className="taskzilla" src={image} alt="image" />
-
-      <button className="landingbtns">Log in</button>
-      <p className="subText">...or if you dont have an account...</p>
-      <button className="landingbtns">Sign up</button>
-    </div>
-
+      <div className="landing">
+        <img className="taskzilla" src={image} alt="image" />
+        <Link to="/client/src/LogInPage/Login.jsx">
+          <button className="landingbtns">Log in</button>
+        </Link>{" "}
+        <p className="subText">...or if you dont have an account...</p>
+        <Link to="/client/src/Login/SignUp.jsx">
+          <button className="landingbtns">Sign up</button>
+        </Link>
+      </div>
     </>
   );
 };
--- client/src/LogInPage/Login.jsx
+++ client/src/LogInPage/Login.jsx
@@ -1,27 +1,47 @@
 import React, { useState } from "react";
 import styles from "./Login.css";
-
+import { Link } from "react-router-dom";
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
 
-    return (
-        <div className="auth-form-container">
-            <img className="Logo" src="Logo.png" alt="" />
-            <h2>Welcome to TaskZilla</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
-    )
-}
+  return (
+    <div className="auth-form-container">
+      <img className="Logo" src="Logo.png" alt="" />
+      <h2>Welcome to TaskZilla</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button className="loginpagebtns" type="submit">Log In</button>
+      </form>
+      <button
+        className="link-btn loginpagebtns"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
+export default Login;
